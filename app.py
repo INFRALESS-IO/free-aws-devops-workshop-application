ab9c43db3030198cb5f01e0a5608485a532ab0a5
Carlos Santini <git@digitalassemb.ly>
2024-03-14 06:33:27 +1000

    fix dockerfile

--- app.py
+++ app.py
@@ -2,14 +2,21 @@ import http.server
 import socketserver
 import os
 
-# Retrieve environment variable or use a default value if it's not set
-HELLO_WORLD_TEXT = os.getenv('HELLO_WORLD_TEXT', 'This is a default message')
-
 class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
-        # Prepare the HTML content
-        html_content = f"<html><body><h1>Hello World! {HELLO_WORLD_TEXT}</h1></body></html>".encode('utf-8')
-        
+        # Environment variable to append to the message, with a default
+        hello_world_text = os.getenv('HELLO_WORLD_TEXT', 'default text')
+
+        # HTML content to be served
+        html_content = f"""
+        <html>
+        <head><title>Sample App</title></head>
+        <body>
+        <h1>Hello, World! {hello_world_text}</h1>
+        </body>
+        </html>
+        """.encode('utf-8')
+
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
@@ -18,5 +25,5 @@ class Handler(http.server.SimpleHTTPRequestHandler):
 PORT = 8000
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print(f"HELLO WORLD is being served at port {PORT}")
+    print("Serving at port", PORT)
     httpd.serve_forever()
